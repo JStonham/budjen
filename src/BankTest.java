1a48f7c72ac48b6ccf9bd1eb21bb4d3914dd3d89
Jen Stonham <jastonham@gmail.com>
2018-07-19 20:20:01 +0100

    create simple successful test

---
+++ src/BankTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+// Make assertions and make runnable test methods.
+//Must be public, void and have no input arguments
+//Must be annotated with @Test
+//test driven development
+public class BankTest {
+    @Test
+    public void test() {
+    }
+}
