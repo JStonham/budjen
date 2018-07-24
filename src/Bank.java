9e43236cb1a146107e342056937ea4eedacdf4c3
Jen Stonham <jastonham@gmail.com>
2018-07-24 19:05:18 +0100

    create classes bank and banktest

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,8 @@
 public class Bank {
+    private long balance;
+
+    public long add(Transaction transaction) {
+        balance = balance + transaction.getMoney();
+        return balance;
+    }
 }
--- src/BankTest.java
+++ src/BankTest.java
@@ -1,11 +1,30 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 // Make assertions and make runnable test methods.
 //Must be public, void and have no input arguments
 //Must be annotated with @Test
 //test driven development
 public class BankTest {
+    Bank bank = new Bank();//<Type> <variable name> = new <Constructor call>;
+
     @Test
-    public void test() {
+    public void testZero() {
+        Transaction transaction = makeTransaction(0);
+        assertEquals(0, bank.add(transaction));
     }
+
+    @Test
+    public void testFive() {
+        long balance = bank.add(makeTransaction(5));
+        assertEquals(5, balance);
+    }
+
+    private Transaction makeTransaction(long money) {
+        Transaction transaction = new Transaction();
+        transaction.setMoney(money);
+        return transaction;
+    }
+
 }
