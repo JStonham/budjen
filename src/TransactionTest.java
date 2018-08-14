ce7b964bb358741b1841159a6c1d1baff06b1ff7
Jen Stonham <jastonham@gmail.com>
2018-08-14 20:44:33 +0100

    Moved methods to end of tests. Removed given no data returns message and given empty description returns description.

--- src/TransactionTest.java
+++ src/TransactionTest.java
@@ -26,12 +26,6 @@ public class TransactionTest {
         assertEquals(10, money);
     }
 
-    private Transaction makeTransaction() {
-        Transaction transaction = new Transaction();
-        transaction.setMoney(10);
-        return transaction;
-    }
-
     @Test
     public void givenNegativeMoney_returnsNegativeMonetaryValue() {
         long money = makeNegativeTransaction().getMoney();
@@ -44,12 +38,6 @@ public class TransactionTest {
         assertEquals("You have spent £20", summary);
     }
 
-    private Transaction makeNegativeTransaction() {
-        Transaction transaction = new Transaction();
-        transaction.setMoney(-20);
-        return transaction;
-    }
-
     @Test
     public void givenZeroMoney_returnsZeroMonetaryValue() {
         long money = new Transaction().getMoney();
@@ -57,33 +45,9 @@ public class TransactionTest {
     }
 
     @Test
-    public void givenZeroMoney_returnsMonetaryDescription() {
-        String summary = new Transaction().getSummary();
-        assertEquals("No data.", summary);
-    }
-
-    @Test
     public void givenDescription_returnsDescription() {
         String description = makeDescription().getDescription();
         assertEquals("Rent",description);
-    }
-
-    private Transaction makeDescription() {
-        Transaction transaction = new Transaction();
-        transaction.setDescription("Rent");
-        return transaction;
-    }
-
-    @Test
-    public void givenEmptyDescription_returnsDescription() {
-        String description = makeEmptyDescription().getDescription();
-        assertEquals("",description);
-    }
-
-    private Transaction makeEmptyDescription() {
-        Transaction transaction = new Transaction();
-        transaction.setDescription("");
-        return transaction;
     }
 
     @Test
@@ -108,25 +72,22 @@ public class TransactionTest {
         assertEquals(today, transaction.getDate());
     }
 
-    /**
-     * Peter's test to highlight a bug when transaction date is set to null. Need to set the date during the setDate(null)
-     * method rather than during the getDate() method.
-     */
-    @Test
-    public void givenNoDate_assertTransactionDateIsTheDateTheTransactionWasCreated() {
-        Date transactionCreated = new Date();
-        Transaction transaction = makeTransaction();
-        transaction.setDate(null);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            Assert.fail("Interrupted");
-        }
 
-        // Make a Date which is just after the transaction date was set to null
-        Date justAfterTransactionDateSetToNull = new Date();
-        justAfterTransactionDateSetToNull.setTime(transactionCreated.getTime() + 10);
-        // Check that the Date of the transaction (when set to null) is the time the Date was set, not the time we get the Date.
-        assertTrue(transaction.getDate().before(justAfterTransactionDateSetToNull));
+    private Transaction makeTransaction() {
+        Transaction transaction = new Transaction();
+        transaction.setMoney(10);
+        return transaction;
+    }
+
+    private Transaction makeNegativeTransaction() {
+        Transaction transaction = new Transaction();
+        transaction.setMoney(-20);
+        return transaction;
+    }
+
+    private Transaction makeDescription() {
+        Transaction transaction = new Transaction();
+        transaction.setDescription("Rent");
+        return transaction;
     }
 }
