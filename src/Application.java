5a9ffd1d5cb38260767532642f477c9027368293
jstonham <jennifer.anne.heath@gmail.com>
2018-09-11 20:33:55 +0100

    Return Transaction Data and Order Data Chronologically

--- src/Application.java
+++ src/Application.java
@@ -1,16 +1,27 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Application {
 
     public void start() {
-        Transaction[] transactions = new TransactionData().getTransactionData();
+        Transaction[] transactions = getData();
         System.out.println();
     }
 
     public Transaction[] getData() {
-        return null;
+        return new TransactionData().getTransactionData();
     }
 
     public Transaction[] orderTransactions(Transaction[] unorderedTransactions) {
-        return null;
+        List<Transaction> transactionList = Arrays.asList(unorderedTransactions);
+        transactionList.sort(new Comparator<Transaction>() {
+            @Override
+            public int compare(Transaction o1, Transaction o2) {
+                return o1.getDate().compareTo(o2.getDate());
+            }
+        });
+        return transactionList.toArray(new Transaction[unorderedTransactions.length]);
     }
 
     public void print() {
