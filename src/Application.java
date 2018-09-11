4859c0a586364a968c2847b749bc2177be2d38fb
jstonham <jennifer.anne.heath@gmail.com>
2018-09-11 18:54:24 +0100

    Created Budjen class and skeleton of Application class.

---
+++ src/Application.java
@@ -0,0 +1,20 @@
+public class Application {
+
+    public void start() {
+        Transaction[] transactions = new TransactionData().getTransactionData();
+        System.out.println();
+    }
+
+    public Transaction[] getData() {
+        return null;
+    }
+
+    public Transaction[] orderTransactions(Transaction[] unorderedTransactions) {
+        return null;
+    }
+
+    public void print() {
+
+    }
+    
+}
