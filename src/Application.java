2cb3715409bf8f808d99d8ae86799ec7c5b8eb5c
jstonham <jennifer.anne.heath@gmail.com>
2018-09-13 20:29:04 +0100

    Refactor Application formatPounds method

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,3 @@
-import sun.java2d.loops.TransformBlit;
-
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -10,30 +7,21 @@ import java.util.List;
 public class Application {
 
     public void start() {
-        Transaction[] transactions = getData();
+        Transaction[] transactions = new TransactionData().getTransactionData();
         Transaction[] orderedTransactions = orderTransactions(transactions);
         for (Transaction transaction : orderedTransactions) {
             print(transaction);
         }
     }
 
-    public Transaction[] getData() {
-        return new TransactionData().getTransactionData();
-    }
-
     public Transaction[] orderTransactions(Transaction[] unorderedTransactions) {
         List<Transaction> transactionList = Arrays.asList(unorderedTransactions);
-        transactionList.sort(new Comparator<Transaction>() {
-            @Override
-            public int compare(Transaction o1, Transaction o2) {
-                return o1.getDate().compareTo(o2.getDate());
-            }
-        });
+        transactionList.sort(Comparator.comparing(Transaction::getDate));
         return transactionList.toArray(new Transaction[unorderedTransactions.length]);
     }
 
     public void print(Transaction transaction) {
-        System.out.println(transaction.getType()+", "+transaction.getDescription()+", "+formatPounds(transaction.getMoney())+", "+formatDate(transaction.getDate()));
+        System.out.println(transaction.getType()+", "+transaction.getDescription()+", "+formatPounds(transaction.getMoney(),transaction.getType())+", "+formatDate(transaction.getDate()));
     }
 
     private String formatDate(Date date) {
@@ -42,17 +30,11 @@ public class Application {
         return simpleDateFormat.format(date);
     }
 
-    public String formatPounds(long money) {
-        if (money == 0) {
-            return "0";
+    public String formatPounds(long money, final TransactionType type) {
+        final Currency currency = new Currency();
+        if (type == TransactionType.DEBIT) {
+            money = -money;
         }
-        if (money <0) {
-            final long remainder = (money % 100) * -1;
-            final String pence = remainder < 10 ? "0" + remainder : "" + remainder;
-            return "£" + ((money / 100) * -1) + "." + pence;
-        }
-        final long remainder = money % 100;
-        final String pence = remainder < 10 ? "0" + remainder : "" + remainder;
-        return "£" + (money / 100) + "." + pence;
+        return currency.formatPounds(money);
     }
 }
