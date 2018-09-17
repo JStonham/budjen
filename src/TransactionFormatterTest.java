ae52bf45cc6bc4e7e57a58c39dec7f00cf3b1a93
jstonham <jennifer.anne.heath@gmail.com>
2018-09-17 21:30:07 +0100

    Tests for TransactionFormatter.

---
+++ src/TransactionFormatterTest.java
@@ -0,0 +1,56 @@
+import org.junit.Test;
+
+import java.time.*;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class TransactionFormatterTest {
+
+    private TransactionFormatter target = new TransactionFormatter();
+
+    @Test
+    public void testTransactionFormatterStringForDebit() {
+
+        TransactionType type = TransactionType.DEBIT;
+        String description = "Test";
+        long money = -400;
+        Date date = Date.from(LocalDate.of(2000, Month.JANUARY, 1)
+                                        .atTime(LocalTime.NOON)
+                                        .toInstant(ZoneOffset.UTC));
+
+        final Transaction transaction = new Transaction();
+        transaction.setType(type);
+        transaction.setDescription(description);
+        transaction.setMoney(money);
+        transaction.setDate(date);
+
+        String transactionAsAString = target.format(transaction);
+        assertNotNull(transactionAsAString);
+        assertNotEquals("",transactionAsAString);
+        assertEquals("DEBIT, Test, £4.00, 2000-01-01", transactionAsAString);
+    }
+
+    @Test
+    public void testTransactionFormatterStringForCredit() {
+
+        TransactionType type = TransactionType.CREDIT;
+        String description = "Test";
+        long money = 7000;
+        Date date = Date.from(LocalDate.of(2010, Month.DECEMBER, 31)
+                .atTime(LocalTime.NOON)
+                .toInstant(ZoneOffset.UTC));
+
+        final Transaction transaction = new Transaction();
+        transaction.setType(type);
+        transaction.setDescription(description);
+        transaction.setMoney(money);
+        transaction.setDate(date);
+
+        String transactionAsAString = target.format(transaction);
+        assertNotNull(transactionAsAString);
+        assertNotEquals("",transactionAsAString);
+        assertEquals("CREDIT, Test, £70.00, 2010-12-31", transactionAsAString);
+    }
+
+}
