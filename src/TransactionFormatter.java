13c1cfaa9194a45cf2b4d2ceef8d7287959e9f99
jstonham <jennifer.anne.heath@gmail.com>
2018-09-17 21:42:11 +0100

    Transaction Formatter TDD green.

--- src/TransactionFormatter.java
+++ src/TransactionFormatter.java
@@ -1,6 +1,27 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TransactionFormatter {
 
     public String format(Transaction transaction) {
-        return null;
+        TransactionType type = transaction.getType();
+        String description = transaction.getDescription();
+        String money = formatPounds(transaction.getMoney(),transaction.getType());
+        String date = formatDate(transaction.getDate());
+        return type+", "+description+", "+money+", "+date;
+    }
+
+    private String formatPounds(long money, final TransactionType type) {
+        final Currency currency = new Currency();
+        if (type == TransactionType.DEBIT) {
+            money = -money;
+        }
+        return currency.formatPounds(money);
+    }
+
+    private String formatDate(Date date) {
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        return simpleDateFormat.format(date);
     }
 }
