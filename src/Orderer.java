ab818df121349b7e1fa74981aab55cde6e18deba
jstonham <jennifer.anne.heath@gmail.com>
2018-09-19 20:38:41 +0100

    Orderer TDD Green

--- src/Orderer.java
+++ src/Orderer.java
@@ -1,2 +1,12 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Orderer {
+
+    public Transaction[] orderTheTransactions(Transaction[] unorderedTransactions) {
+        List<Transaction> transactionList = Arrays.asList(unorderedTransactions);
+        transactionList.sort(Comparator.comparing(Transaction::getDate));
+        return transactionList.toArray(new Transaction[unorderedTransactions.length]);
+    }
 }
