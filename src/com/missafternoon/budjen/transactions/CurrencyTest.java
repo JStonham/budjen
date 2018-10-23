93c799bb2d8f328375274779703cb17073611e7c
jstonham <jennifer.anne.heath@gmail.com>
2018-10-23 19:21:10 +0100

    Changed test class and methods using junit back to public.

--- src/com/missafternoon/budjen/transactions/CurrencyTest.java
+++ src/com/missafternoon/budjen/transactions/CurrencyTest.java
@@ -4,26 +4,26 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-class CurrencyTest {
+public class CurrencyTest {
 
     private Currency target = new Currency();
 
     @Test
-    private void givenZero_givesZero() {
+    public void givenZero_givesZero() {
         runTest("0", 0);
     }
 
     @Test
-    private void given5Pounds_gives5Pounds() {
+    public void given5Pounds_gives5Pounds() {
         runTest("£5.00", 500);
     }
 
     @Test
-    private void given42Pence_gives42Pence() {
+    public void given42Pence_gives42Pence() {
         runTest("£0.42", 42);
     }
 
-    private void runTest(final String s, final int i) {
+    public void runTest(final String s, final int i) {
         assertEquals(s, target.formatPounds(i));
     }
 
