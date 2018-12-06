6030af0ba830019d9d6d77da67643045d264f9d0
jstonham <jennifer.anne.heath@gmail.com>
2018-12-06 20:13:01 +0000

    Fix Arrays as List bug.

--- src/main/java/com/missafternoon/budjen/transactions/DefaultTransactionData.java
+++ src/main/java/com/missafternoon/budjen/transactions/DefaultTransactionData.java
@@ -2,35 +2,37 @@ package com.missafternoon.budjen.transactions;
 
 import com.missafternoon.budjen.formatting.DateConverter;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DefaultTransactionData implements TransactionData {
 
     private DateConverter dateConverter = new DateConverter();
+    private List<Transaction> transactions = new ArrayList<>();
+
+    public DefaultTransactionData() {
+        transactions.add(credit("Dress Refund", 2999, "2017-09-28"));
+        transactions.add(credit("Pocket Money", 250, "2013-11-04"));
+        transactions.add(credit("Lottery winnings", 5600000, "2017-12-25"));
+        transactions.add(credit("TV Appearance", 100000, "2018-01-01"));
+        transactions.add(credit("Book Deal", 500000, "2018-02-28"));
+        transactions.add(credit("Poker winnings", 80000, "2017-05-15"));
+        transactions.add(credit("Paper Round", 2300, "2014-03-17"));
+        transactions.add(credit("Let flat", 420000, "2018-06-08"));
+
+        transactions.add(debit("Lottery Ticket", -300, "2017-12-21"));
+        transactions.add(debit("Train Ticket", -12330, "2017-07-16"));
+        transactions.add(debit("Dog", -700000, "2015-09-30"));
+        transactions.add(debit("Dog toys", -1549, "2015-10-02"));
+        transactions.add(debit("Blue Audi", -4000000, "2018-04-03"));
+        transactions.add(debit("Small Yacht", -5900099, "2018-05-07"));
+        transactions.add(debit("Plastic Surgery", -2500000, "2018-06-13"));
+        transactions.add(debit("Skiing holiday", -510000, "2018-01-28"));
+    }
 
     @Override
     public List<Transaction> getTransactionData() {
-        return Arrays.asList(
-                credit("Dress Refund", 2999, "2017-09-28"),
-                credit("Pocket Money", 250, "2013-11-04"),
-                credit("Lottery winnings", 5600000, "2017-12-25"),
-                credit("TV Appearance", 100000, "2018-01-01"),
-                credit("Book Deal", 500000, "2018-02-28"),
-                credit("Poker winnings", 80000, "2017-05-15"),
-                credit("Paper Round", 2300, "2014-03-17"),
-                credit("Let flat", 420000, "2018-06-08"),
-
-                debit("Lottery Ticket", -300, "2017-12-21"),
-                debit("Train Ticket", -12330, "2017-07-16"),
-                debit("Dog", -700000, "2015-09-30"),
-                debit("Dog toys", -1549, "2015-10-02"),
-                debit("Blue Audi", -4000000, "2018-04-03"),
-                debit("Small Yacht", -5900099, "2018-05-07"),
-                debit("Plastic Surgery", -2500000, "2018-06-13"),
-                debit("Skiing holiday", -510000, "2018-01-28")
-        );
-
+        return transactions;
     }
 
     private Transaction credit(final String description, final long money, final String date) {
