1afa7a83eb6fcf23ee53766b1f73ffc7dfb33f82
jstonham <jennifer.anne.heath@gmail.com>
2019-01-02 19:56:38 +0000

    Compare every component of transactions with valid json in String to Object test.

--- src/test/java/com/missafternoon/budjen/transactions/JSONInterpreterTest.java
+++ src/test/java/com/missafternoon/budjen/transactions/JSONInterpreterTest.java
@@ -7,9 +7,11 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class JSONInterpreterTest {
 
@@ -30,9 +32,39 @@ public class JSONInterpreterTest {
     @Test
 
     public void readJSONStringIntoJavaObject() {
-        String string = "CREDIT, Work, £3000.00, 2018-11-14, DEBIT, Fuel, £25.00, 2018-12-16";
-        List<Transaction> transactions = target.convertToTransactions(string);
-        assertEquals(2, transactions.size());
+        String json = "[" +
+                "{\"type\":\"DEBIT\",\"description\":\"Fuel\",\"money\":-2500,\"date\":\"2018-12-16\"}," +
+                "{\"type\":\"CREDIT\",\"description\":\"Work\",\"money\":300000,\"date\":\"2018-11-14\"}" +
+                "]";
+
+        DateConverter dateConverter = new DateConverter();
+        Transaction expectedTransaction1 = new Transaction();
+        expectedTransaction1.setType(TransactionType.DEBIT);
+        expectedTransaction1.setDescription("Fuel");
+        expectedTransaction1.setMoney(-2500);
+        expectedTransaction1.setDate(dateConverter.convert("2018-12-16"));
+        Transaction expectedTransaction2 = new Transaction();
+        expectedTransaction2.setType(TransactionType.CREDIT);
+        expectedTransaction2.setDescription("Work");
+        expectedTransaction2.setMoney(300000);
+        expectedTransaction2.setDate(dateConverter.convert("2018-11-14"));
+        List<Transaction> expectedTransactions = Arrays.asList(expectedTransaction1, expectedTransaction2);
+
+        List<Transaction> transactions = target.convertToTransactions(json);
+
+        assertEquals(expectedTransactions.size(), transactions.size());
+        for (int i = 0; i < expectedTransactions.size(); i++) {
+            Transaction expected = expectedTransactions.get(i);
+            Transaction actual = transactions.get(i);
+            assertNotNull(actual);
+            assertEquals(expected.getType(), actual.getType());
+            assertEquals(expected.getDescription(), actual.getDescription());
+            assertEquals(expected.getMoney(), actual.getMoney());
+            assertEquals(expected.getDate(), actual.getDate());
+        }
+
+        // TODO: Investigate overriding .equals for Transaction instead, replacing the above block with the below line
+        //assertEquals(expectedTransactions, transactions);
     }
 
     private class TestTransactionData implements TransactionData {
