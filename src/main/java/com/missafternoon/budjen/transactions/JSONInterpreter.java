66c6364a5f43415d8dc3ce34e25924b831abbf18
jstonham <jennifer.anne.heath@gmail.com>
2019-01-02 20:19:49 +0000

    Refactor JSON Interpreter caught exceptions.

--- src/main/java/com/missafternoon/budjen/transactions/JSONInterpreter.java
+++ src/main/java/com/missafternoon/budjen/transactions/JSONInterpreter.java
@@ -1,8 +1,6 @@
 package com.missafternoon.budjen.transactions;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -19,25 +17,16 @@ public class JSONInterpreter {
     public String convertToString(List<Transaction> transactions) {
         try {
             return mapper.writeValueAsString(transactions);
-        } catch (JsonGenerationException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
         }
         throw new RuntimeException();
     }
 
     public List<Transaction> convertToTransactions(String string) {
         try {
-            return mapper.readValue(string, new TypeReference<List<Transaction>>() {});
-        } catch (JsonGenerationException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
+            return mapper.readValue(string, new TypeReference<List<Transaction>>() {
+            });
         } catch (IOException e) {
-            e.printStackTrace();
         }
         throw new RuntimeException();
     }
--- src/main/java/com/missafternoon/budjen/transactions/Transaction.java
+++ src/main/java/com/missafternoon/budjen/transactions/Transaction.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.missafternoon.budjen.formatting.Currency;
 
 import java.util.Date;
-import java.util.TimeZone;
 
 public class Transaction {
 
