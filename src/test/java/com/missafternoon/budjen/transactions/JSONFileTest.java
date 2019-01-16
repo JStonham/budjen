14f71efe97ee71333cfe7e376a52ccec8552d89a
jstonham <jennifer.anne.heath@gmail.com>
2019-01-16 21:24:54 +0000

    Addressed review comments on Create JSON File in Test #32

--- src/test/java/com/missafternoon/budjen/transactions/JSONFileTest.java
+++ src/test/java/com/missafternoon/budjen/transactions/JSONFileTest.java
@@ -6,17 +6,49 @@ import org.junit.Test;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
 
 public class JSONFileTest {
 
     private final ObjectMapper mapper = new ObjectMapper();
+    private BufferedWriter writer;
 
     @Test
-    public void whenWriteStringUsingBufferedWriter_thenCreateFile()
+    public void whenWriteStringUsingBufferedWriter_thenCreateFile() {
+        String home = System.getProperty("user.home");
+        String json = "{}";
+        try {
+            writer = new BufferedWriter(new FileWriter(home + "/.config/budjen/transactions.json"));
+            writer.write(json);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Test
+    public void givenUsingJava7_whenWritingToFile_thenCorrect()
             throws IOException {
-        String json = "";
-        BufferedWriter writer = new BufferedWriter(new FileWriter("/home/jen/.config/budjen/transactions.json"));
-        writer.write(json);
-        writer.close();
+        String home = System.getProperty("user.home");
+        String str = "{}";
+
+        Path path = Paths.get(home + "/.config/budjen/transactions.json");
+        byte[] strToBytes = str.getBytes();
+
+        Files.write(path, strToBytes);
+
+        String read = Files.readAllLines(path).get(0);
+        assertEquals(str, read);
     }
 }
