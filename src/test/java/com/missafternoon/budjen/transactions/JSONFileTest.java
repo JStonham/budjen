a2112f53eb627ff8f4ad3f52acf181f1b36a0411
jstonham <jennifer.anne.heath@gmail.com>
2019-01-17 19:27:47 +0000

    Create directories if they don't exist.

--- src/test/java/com/missafternoon/budjen/transactions/JSONFileTest.java
+++ src/test/java/com/missafternoon/budjen/transactions/JSONFileTest.java
@@ -1,54 +1,22 @@
 package com.missafternoon.budjen.transactions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static org.junit.Assert.assertEquals;
 
 public class JSONFileTest {
 
-    private final ObjectMapper mapper = new ObjectMapper();
-    private BufferedWriter writer;
-
     @Test
     public void whenWriteStringUsingBufferedWriter_thenCreateFile() {
-        String home = System.getProperty("user.home");
-        String json = "{}";
-        try {
-            writer = new BufferedWriter(new FileWriter(home + "/.config/budjen/transactions.json"));
-            writer.write(json);
+        final String dir = System.getProperty("user.home") + "/.test/budjen/";
+        new File(dir).mkdirs();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(dir + "transactions.json"))) {
+            writer.write("{}");
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (writer != null) {
-                    writer.close();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         }
-    }
-
-    @Test
-    public void givenUsingJava7_whenWritingToFile_thenCorrect()
-            throws IOException {
-        String home = System.getProperty("user.home");
-        String str = "{}";
-
-        Path path = Paths.get(home + "/.config/budjen/transactions.json");
-        byte[] strToBytes = str.getBytes();
-
-        Files.write(path, strToBytes);
-
-        String read = Files.readAllLines(path).get(0);
-        assertEquals(str, read);
     }
 }
